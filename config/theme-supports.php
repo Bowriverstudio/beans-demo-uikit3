01ce5e78bf8649a5c440a02db4986e4c63dc75c6
Maurice Tadros <maurice.tadros@gmail.com>
2019-12-15 19:50:10 -0700

    Updated theme supports to just bean related items

--- config/theme-supports.php
+++ config/theme-supports.php
@@ -7,16 +7,12 @@
  */
 
 
-$default_theme_support = require get_template_directory() . '/config/theme-supports.php';
 
-$child_theme = array(
+return array(
 	// Beans specific.
     'offcanvas-menu' => '',
     'beans-default-styling' => '',
     'beans-template-landing-page' => '',
 );
-
-return array_merge($default_theme_support, $child_theme);
-
 
 
